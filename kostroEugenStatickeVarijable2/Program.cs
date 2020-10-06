129c36494514bd39dab1ee1fdc1aa2eba48e588e
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2020-10-06 11:04:10 +0200

    Add files via upload

---
+++ kostroEugenStatickeVarijable2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kostroEugenStatickeVarijable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PrvaKlasa x = new PrvaKlasa("Eugen", "Koštro");
+            Console.WriteLine(x.ToString());
+            PrvaKlasa y = new PrvaKlasa("Pero", "Perić");
+            Console.WriteLine(y.ToString());
+            PrvaKlasa z = new PrvaKlasa();
+            
+
+            z.Ime = "Đuro";
+            z.Prezime = "Đurić";
+            Console.WriteLine(z.ToString());
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ kostroEugenStatickeVarijable2/PrvaKlasa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kostroEugenStatickeVarijable2
+{
+    class PrvaKlasa
+    {
+        private static int brojac;
+        private string ime;
+        private string prezime;
+
+        public PrvaKlasa()
+        {
+            brojac++;
+
+        }
+        public PrvaKlasa(string ime, string prezime)
+        {
+            brojac++;
+            this.ime = ime;
+            this.prezime = prezime;
+        }
+
+        public string Ime { get => ime; set => ime = value; }
+        public string Prezime { get => prezime; set => prezime = value; }
+
+        public int Brojac()
+        {
+            return brojac;
+        }
+
+        public override string ToString()
+        {
+            string ispis = "Redni broj: " + brojac+", "+Ime+" "+prezime;
+
+            return ispis;
+
+            
+            
+        }
+    }
+}
